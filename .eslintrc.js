e993c0a6eccb8d1d2a870183b54fe32b1ad9030b
pro-xiaoy <pro-xiaoy@xiaoy-mbp.local>
2020-04-10 10:56:02 +0800

    $1

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,6 @@ module.exports = {
 
   // add your custom rules here
   'rules': {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     quotes: ['error', 'single'], //强制使用单引号
     semi: ['error', 'never'], //强制不使用分号结尾
